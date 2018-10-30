b6036328cccacc8ecdc827941ef0fbe60249518a
Your NameLukasz <you@example.com>
2018-10-30 14:44:00 +0100

    New

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,66 @@
+
+ 
+ 
+ 
+ const cruise = document.querySelector('#main');
+  const ahrens = cruise.offsetTop;
+
+  function cruiseamerica() {
+    if(window.scrollY >= ahrens) {
+      document.body.style.paddingTop = cruise.offsetHeight + 'px';
+      document.body.classList.add('fixed-nav');
+    } else {
+      document.body.style.paddingTop = 0;
+      document.body.classList.remove('fixed-nav');
+    }
+
+  }
+
+  window.addEventListener('scroll', cruiseamerica);
+
+
+
+  function myFunctions() {
+    document.getElementById('comment-rachel').classList.remove('opend');
+    document.getElementById('comment-asa').classList.add('opend');
+
+    document.getElementById('rachelpic').src = "/rachel0.png";
+    document.getElementById('asapic').src = "/asa.png";
+
+    document.getElementById('comment-asa').style.display = 'block';
+    document.getElementById('comment-rachel').style.display = 'none';
+  }
+
+
+  function myFunctions2() {
+    document.getElementById('comment-rachel').classList.add('opend');
+    document.getElementById('comment-asa').classList.remove('opend');
+    document.getElementById('comment-asa').classList.add('close');
+
+    document.getElementById('rachelpic').src = "/rachel.png";
+    document.getElementById('asapic').src = "/asa0.png";
+
+    document.getElementById('comment-asa').style.display = 'none';
+    document.getElementById('comment-rachel').style.display = 'block';
+  }
+
+
+
+
+
+function myFunction() {
+  var element = document.getElementById("newul");
+  element.classList.toggle('show');
+}
+
+var button = document.querySelector('.menu-icon');
+button.addEventListener('click', function (){
+button.classList.toggle('open');
+});
+
+
+
+
+
+
+
